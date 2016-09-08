ba95edad953bea9770ce69a31ee1a54ff743addc
Sam Jacob <Sam.Jacob@synechron.com>
2016-09-08 19:43:46 +0530

    test|mysql -v1

---
+++ mysql.r
@@ -0,0 +1,6 @@
+# install.packages("RMySQL")
+library(RMySQL)
+mydb= dbConnect(MySQL(),user="root",password="root",dbname="sakila",host="localhost")
+dbListTables(mydb)
+rs <- dbSendQuery(mydb,"select * from actor")
+rows <- fetch(rs,n=-1)
